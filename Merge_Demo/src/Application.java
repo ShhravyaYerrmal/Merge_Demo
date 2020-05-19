d242931bca1fc98f22effb99663f2a03262688a1
Aayushi-yadav <aayushi8218@gmail.com>
2020-05-19 19:34:33 +0530

    First comit

---
+++ Merge_Demo/src/Application.java
@@ -0,0 +1,16 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		/*
+		 * A comment
+		 * 
+		 * Another change
+		 * 
+		 * change A 
+		 * change B
+		 */
+		System.out.println("An Android Application");
+	}
+
+}
